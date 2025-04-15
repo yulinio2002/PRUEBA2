a199d1da1d136b73214501f1f184bfc964c2cff1
Yulinio Zavala <yulinio.zavala@utec.edu.pe>
2025-04-15 14:36:39 -0500

    first commit

---
+++ src/main/java/com/example/pruebahack/PruebaHackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebahack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaHackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pruebahack/PruebaHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebahack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
